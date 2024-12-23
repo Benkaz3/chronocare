3c26bf35bd7dafdc853824c066fa0f37972927c3
Jinsorei <eeijnl@gmail.com>
2024-12-23 12:27:21 +0700

    fixing deployment failure

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-import '@mui/material/styles'; // Import necessary MUI styles
+import '@mui/material/styles';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
